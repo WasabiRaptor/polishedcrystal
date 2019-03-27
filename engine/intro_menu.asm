8e3117b452d4e91823f149defacc9e550710cf04
Thalins Genohan <thalinsgenohan@gmail.com>
2019-03-27 16:54:55 -0400

    Fixed crash in intro

--- engine/intro_menu.asm
+++ engine/intro_menu.asm
@@ -699,8 +699,6 @@ endc
 	ld c, 10
 	call DelayFrames
 
-	ld de, ElmName
-
 	ld hl, ElmText6
 	call PrintText
 
@@ -715,7 +713,6 @@ endc
 	call GetCGBLayout
 	call InitIntroGradient
 	call Intro_RotatePalettesLeftFrontpic
-
 
 	ld hl, ElmText7
 	jp PrintText
