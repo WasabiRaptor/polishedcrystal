9bb3e016246fd7a629f5144a4089c030fbb565cd
WasabiRaptor <ketchupraptor@gmail.com>
2019-03-27 16:45:53 -0400

    fix crash with Nameplate

--- engine/intro_menu.asm
+++ engine/intro_menu.asm
@@ -669,10 +669,11 @@ if !DEF(DEBUG)
 	call InitIntroGradient
 	call Intro_RotatePalettesLeftFrontpic
 
+	ld de, ElmName
 	ld hl, ElmText2
 	call PrintNamedText
 	ld hl, ElmText4
-	call PrintNamedText
+	call PrintText
 	ld c, 15
 	call FadeToWhite
 	call ClearTileMap
@@ -688,6 +689,7 @@ if !DEF(DEBUG)
 	call InitIntroGradient
 	call Intro_RotatePalettesLeftFrontpic
 
+	ld de, ElmName
 	ld hl, ElmText5
 	call PrintNamedText
 endc
@@ -697,8 +699,10 @@ endc
 	ld c, 10
 	call DelayFrames
 
+	ld de, ElmName
+
 	ld hl, ElmText6
-	call PrintNamedText
+	call PrintText
 
 	call NamePlayer
 
@@ -712,8 +716,9 @@ endc
 	call InitIntroGradient
 	call Intro_RotatePalettesLeftFrontpic
 
+
 	ld hl, ElmText7
-	jp PrintNamedText
+	jp PrintText
 
 ElmText1: ; 0x6045
 	text_jump _ElmText1
