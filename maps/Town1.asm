9fcfd07f7f0d65827442934db70f15450702e6af
WasabiRaptor <ketchupraptor@gmail.com>
2019-06-14 22:39:01 -0400

    Form based Evolution functioning

--- maps/Town1.asm
+++ maps/Town1.asm
@@ -20,12 +20,15 @@ Town1_MapScriptHeader:
 
 MysteryDungeonEnterScript:
 	opentext
-	givepoke VULPIX, 5, FIRE_STONE, MALE | ALOLAN, LUXURY_BALL, %10000000, $2345, IndigoName, IndigoOT
-	givepoke VULPIX, 5, ICE_STONE, FEMALE | KANTONIAN, LUXURY_BALL, %10000000, $2345, IndigoName, IndigoOT
+	givepoke VULPIX, 5, ICE_STONE, MALE | ALOLAN, LUXURY_BALL, %10000000, $2345, AlolaName, IndigoOT
+	givepoke VULPIX, 5, FIRE_STONE, FEMALE | KANTONIAN, LUXURY_BALL, %10000000, $2345, KantoName, IndigoOT
 	givepoke ZORUA, 10, ORAN_BERRY, MALE, LUXURY_BALL, %10000000, $2345, IndigoName, IndigoOT
 	closetext
 	end
-
+AlolaName:
+	db "Alola@"
+KantoName:
+	db "Kanto@"
 IndigoName:
 	db "Indigo@"
 IndigoOT:
