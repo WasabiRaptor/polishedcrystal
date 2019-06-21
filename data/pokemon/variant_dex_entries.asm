1d766ddc8190e707dc75028eb325807c77548640
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2019-06-21 09:55:35 -0400

    Revert "partway implemented dex entries"
    This reverts commit c20d374da0f41b23507a6d10f91b168dfef7b5ad.

--- data/pokemon/variant_dex_entries.asm
+++
@@ -1,5 +0,0 @@
-VulpixPokedexEntry::  INCLUDE "data/pokemon/dex_entries/vulpix.asm"
-VulpixPokedexEntry::  INCLUDE "data/pokemon/dex_entries/vulpix_alola.asm"
-
-NinetalesPokedexEntry::   INCLUDE "data/pokemon/dex_entries/ninetales.asm"
-NinetalesPokedexEntry::   INCLUDE "data/pokemon/dex_entries/ninetales_alola.asm"
