84b3a100f3432e70a942e4f74fbd90964c6e5a2e
WasabiRaptor <ketchupraptor@gmail.com>
2019-06-28 19:29:17 -0400

    beta form pics

--- data/pokemon/variant_names.asm
+++ data/pokemon/variant_names.asm
@@ -5,3 +5,11 @@ NidorinoNames::
 NidokingNames::
 	db "N","i","d","o","k","i","n","g","@","@"
 	db "N","i","d","o","q","u","e","e","n","@"
+
+SandshrewNames::
+	db "S","a","n","d","s","h","r","e","w","@"
+	db "S","n","o","w","s","h","r","e","w","@"
+
+SandslashNames::
+	db "S","a","n","d","s","l","a","s","h","@"
+	db "S","n","o","w","s","l","a","s","h","@"
