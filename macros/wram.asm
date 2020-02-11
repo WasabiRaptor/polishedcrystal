e981a17502c6eaff7ad483d14e8f2cabca6b85a8
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2020-02-10 21:02:24 -0500

    Made some macros more readable

--- macros/wram.asm
+++ macros/wram.asm
@@ -3,96 +3,96 @@ flag_array: MACRO
 ENDM
 
 box_struct: MACRO
-\1Group::          db ; one next to the other so we can just inc hl to the next
-\1Species::        db
-\1Item::           dw
+\1Group::          ds 1 ; one next to the other so we can just inc hl to the next
+\1Species::        ds 1
+\1Item::           ds 2
 \1Moves::          ds NUM_MOVES
 \1PPUp::
 \1MovesHigh::      ds NUM_MOVES
-\1ID::             dw
+\1ID::             ds 2
 \1Exp::            ds 3
 \1EVs::
-\1HPEV::           db
-\1AtkEV::          db
-\1DefEV::          db
-\1SpdEV::          db
-\1SatEV::          db
-\1SdfEV::          db
+\1HPEV::           ds 1
+\1AtkEV::          ds 1
+\1DefEV::          ds 1
+\1SpdEV::          ds 1
+\1SatEV::          ds 1
+\1SdfEV::          ds 1
 \1DVs::
-\1HPAtkDV::        db
-\1DefSpdDV::       db
-\1SatSdfDV::       db
+\1HPAtkDV::        ds 1
+\1DefSpdDV::       ds 1
+\1SatSdfDV::       ds 1
 \1Personality::
 \1Shiny::
 \1Ability::
-\1Nature::         db
+\1Nature::         ds 1
 \1Gender::
 \1IsEgg::
 \1IsDead::
-\1Form::           db
-\1Happiness::      db
-\1PokerusStatus::  db
+\1Form::           ds 1
+\1Happiness::      ds 1
+\1PokerusStatus::  ds 1
 \1CaughtData::
 \1CaughtGender::
 \1CaughtTime::
-\1CaughtBall::     db
-\1CaughtLevel::    db
-\1CaughtLocation:: db
-\1Level::          db
-\1Status::         db
+\1CaughtBall::     ds 1
+\1CaughtLevel::    ds 1
+\1CaughtLocation:: ds 1
+\1Level::          ds 1
+\1Status::         ds 1
 \1CurPP::          ds NUM_MOVES ; once curPP is moved to the party struct, the box struct can use these bytes for a move's group, they're already using two bits for PP up so even with curpp they would still be needed
-\1HP::             dw
+\1HP::             ds 2
 \1End::
 ENDM
 
 party_struct: MACRO
 	box_struct \1
-\1MaxHP::          dw
+\1MaxHP::          ds 2
 \1Stats:: ; big endian
-\1Attack::         dw
-\1Defense::        dw
-\1Speed::          dw
-\1SpclAtk::        dw
-\1SpclDef::        dw
+\1Attack::         ds 2
+\1Defense::        ds 2
+\1Speed::          ds 2
+\1SpclAtk::        ds 2
+\1SpclDef::        ds 2
 \1StatsEnd::
 ENDM
 
 battle_struct: MACRO
-\1Group::          db
-\1Species::        db
-\1Item::           dw
+\1Group::          ds 1
+\1Species::        ds 1
+\1Item::           ds 2
 \1Moves::          ds NUM_MOVES
 \1PPUp::
 \1MovesHigh::      ds NUM_MOVES
 ; Mon capture assumes personality comes after DVs
 \1DVs::
-\1HPAtkDV::        db
-\1DefSpdDV::       db
-\1SatSdfDV::       db
+\1HPAtkDV::        ds 1
+\1DefSpdDV::       ds 1
+\1SatSdfDV::       ds 1
 \1Personality::
 \1Shiny::
 \1Ability::
-\1Nature::         db
+\1Nature::         ds 1
 \1Gender::
 \1IsEgg::
 \1IsDead::
-\1Form::           db
-\1Happiness::      db
-\1Level::          db
-\1Status::         db
+\1Form::           ds 1
+\1Happiness::      ds 1
+\1Level::          ds 1
+\1Status::         ds 1
 \1CurPP::          ds NUM_MOVES ; once curPP is moved to the party struct, the box struct can use these bytes for a move's group, they're already using two bits for PP up so even with curpp they would still be needed
-\1HP::             dw
-\1MaxHP::          dw
+\1HP::             ds 2
+\1MaxHP::          ds 2
 \1Stats:: ; big endian
-\1Attack::         dw
-\1Defense::        dw
-\1Speed::          dw
-\1SpclAtk::        dw
-\1SpclDef::        dw
+\1Attack::         ds 2
+\1Defense::        ds 2
+\1Speed::          ds 2
+\1SpclAtk::        ds 2
+\1SpclDef::        ds 2
 \1StatsEnd::
 \1Type::
-\1Type1::          db
-\1Type2::          db
+\1Type1::          ds 1
+\1Type2::          ds 1
 \1StructEnd::
 ENDM
 
@@ -112,45 +112,45 @@ ENDM
 
 channel_struct: MACRO
 ; Addreses are wChannel1 (c101).
-\1MusicID::           dw
-\1MusicBank::         db
-\1Flags::             db ; 0:on/off 1:subroutine 3:sfx 4:noise 5:rest
-\1Flags2::            db ; 0:vibrato on/off 2:duty 4:cry pitch
-\1Flags3::            db ; 0:vibrato up/down
-\1MusicAddress::      dw
-\1LastMusicAddress::  dw
-                      dw
-\1NoteFlags::         db ; 5:rest
-\1Condition::         db ; conditional jumps
-\1DutyCycle::         db ; bits 6-7 (0:12.5% 1:25% 2:50% 3:75%)
-\1Intensity::         db ; hi:pressure lo:velocity
+\1MusicID::           ds 2
+\1MusicBank::         ds 1
+\1Flags::             ds 1 ; 0:on/off 1:subroutine 3:sfx 4:noise 5:rest
+\1Flags2::            ds 1 ; 0:vibrato on/off 2:duty 4:cry pitch
+\1Flags3::            ds 1 ; 0:vibrato up/down
+\1MusicAddress::      ds 2
+\1LastMusicAddress::  ds 2
+                      ds 2
+\1NoteFlags::         ds 1 ; 5:rest
+\1Condition::         ds 1 ; conditional jumps
+\1DutyCycle::         ds 1 ; bits 6-7 (0:12.5% 1:25% 2:50% 3:75%)
+\1Intensity::         ds 1 ; hi:pressure lo:velocity
 \1Frequency:: ; 11 bits
-\1FrequencyLo::       db
-\1FrequencyHi::       db
-\1Pitch::             db ; 0:rest 1-c:note
-\1Octave::            db ; 7-0 (0 is highest)
-\1PitchOffset::       db ; raises existing octaves (to repeat phrases)
-\1NoteDuration::      db ; frames remaining for the current note
+\1FrequencyLo::       ds 1
+\1FrequencyHi::       ds 1
+\1Pitch::             ds 1 ; 0:rest 1-c:note
+\1Octave::            ds 1 ; 7-0 (0 is highest)
+\1PitchOffset::       ds 1 ; raises existing octaves (to repeat phrases)
+\1NoteDuration::      ds 1 ; frames remaining for the current note
 \1Field0x16::         ds 1 ; c117
                       ds 1 ; c118
-\1LoopCount::         db
-\1Tempo::             dw
-\1Tracks::            db ; hi:left lo:right
+\1LoopCount::         ds 1
+\1Tempo::             ds 2
+\1Tracks::            ds 1 ; hi:left lo:right
 \1SFXDutyLoop::       ds 1 ; c11d
-\1VibratoDelayCount:: db ; initialized by \1VibratoDelay
-\1VibratoDelay::      db ; number of frames a note plays until vibrato starts
-\1VibratoExtent::     db
-\1VibratoRate::       db ; hi:frames for each alt lo:frames to the next alt
-\1PitchWheelTarget::  dw ; frequency endpoint for pitch wheel
-\1PitchWheelAmount::  db ; c124
-\1PitchWheelAmountFraction:: db ; c125
+\1VibratoDelayCount:: ds 1 ; initialized by \1VibratoDelay
+\1VibratoDelay::      ds 1 ; number of frames a note plays until vibrato starts
+\1VibratoExtent::     ds 1
+\1VibratoRate::       ds 1 ; hi:frames for each alt lo:frames to the next alt
+\1PitchWheelTarget::  ds 2 ; frequency endpoint for pitch wheel
+\1PitchWheelAmount::  ds 1 ; c124
+\1PitchWheelAmountFraction:: ds 1 ; c125
 \1Field0x25::         ds 1 ; c126
                       ds 1 ; c127
-\1CryPitch::          dw
+\1CryPitch::          ds 2
 \1Field0x29::         ds 1
 \1Field0x2a::         ds 2
 \1Field0x2c::         ds 1
-\1NoteLength::        db ; frames per 16th note
+\1NoteLength::        ds 1 ; frames per 16th note
 \1Field0x2e::         ds 1 ; c12f
 \1Field0x2f::         ds 1 ; c130
 \1Field0x30::         ds 1 ; c131
@@ -194,10 +194,10 @@ hof_mon: MACRO
 endm
 
 roam_struct: MACRO
-\1Species::     db
-\1Level::       db
-\1MapGroup::    db
-\1MapNumber::   db
+\1Species::     ds 1
+\1Level::       ds 1
+\1MapGroup::    ds 1
+\1MapNumber::   ds 1
 \1HP::          ds 1
 \1DVs::         ds 3
 \1Personality:: ds 2
@@ -258,13 +258,13 @@ move_struct: MACRO
 endm
 
 slot_reel: MACRO
-\1ReelAction::   db
-\1TilemapAddr::  dw
-\1Position::     db
-\1SpinDistance:: db
-\1SpinRate::     db
-\1OAMAddr::      dw
-\1XCoord::       db
+\1ReelAction::   ds 1
+\1TilemapAddr::  ds 2
+\1Position::     ds 1
+\1SpinDistance:: ds 1
+\1SpinRate::     ds 1
+\1OAMAddr::      ds 2
+\1XCoord::       ds 1
 \1Slot09::       ds 1
 \1Slot0a::       ds 1
 \1Slot0b::       ds 1
