2111389396e80581fa3bed95b508c4471369feb8
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2020-05-23 08:08:02 -0400

    Failed acall/ajp macros; unnecessary farcall error

--- macros/rst.asm
+++ macros/rst.asm
@@ -4,24 +4,26 @@ AddNTimes  EQU $18
 CopyBytes  EQU $20
 JumpTable  EQU $28
 Predef     EQU $30
-FarJP 	   EQU $38
+FarJP      EQU $38
 
 anonbankpush: macro
 	call AnonBankPush
 	db BANK(\1)
 endm
 
-farcall: macro ; bank, address
+farcall: macro
+	assert BANK(@) != BANK(\1), "Unnecessary farcall"
 	rst FarCall
 	dbw BANK(\1), \1
 endm
 
-farjp: macro ; bank, address
+farjp: macro
+	assert BANK(@) != BANK(\1), "Unnecessary farjp"
 	rst FarJP
 	dbw BANK(\1), \1
 endm
 
-homecall: macro ; bank, address
+homecall: macro
 	ldh a, [hROMBank]
 	push af
 if _NARG == 2
@@ -33,4 +35,64 @@ endc
 	call \1
 	pop af
 	rst Bankswitch
+endm
+
+; Auto Call
+; NOT FUNCTIONAL
+; More research needed to see if even possible
+acall: macro
+if _NARG == 2
+COND\@ EQUS "\1"
+	shift
+else
+COND\@ EQUS ""
+endc
+
+if BANK(\1) == BANK(@)
+	call COND\@, \1
+else
+if "{COND\@}" == "z"
+	jr nz, .nofarcall\@
+elif "{COND\@}" == "nz"
+	jr z, , .nofarcall\@
+elif "{COND\@}" == "c"
+	jr nc, .nofarcall\@
+elif "{COND\@}" == "nc"
+	jr c, .nofarcall\@
+endc
+	farcall \1
+.nofarcall\@:
+endc
+endm
+
+; Auto Jump
+; NOT FUNCTIONAL
+; More research needed to see if even possible
+ajp: macro
+if _NARG == 2
+	COND\@ EQUS "\1"
+	shift
+else
+	COND\@ EQUS ""
+endc
+
+if BANK(@) == BANK(\1)
+if \1 <= @+127 && \1 >= @-128
+	jr COND\@, \1
+else
+	jp COND\@, \1
+endc
+else
+if COND\@ == "z"
+	jr nz, .nofarjp\@
+elif COND\@ == "nz"
+	jr z, , .nofarjp\@
+elif COND\@ == "c"
+	jr nc, .nofarjp\@
+elif COND\@ == "nc"
+	jr c, .nofarjp\@
+endc
+	farjp \1
+.nofarjp\@:
+endc
 endm
